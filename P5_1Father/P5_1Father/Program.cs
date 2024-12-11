2352ee967b16b08e77bc748267108550bfe01203
Jama-student <jamafahad097@gmail.com>
2024-12-11 20:13:54 +0100

    Add files via upload

---
+++ P5_1Father/P5_1Father/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P5_1Father;
+
+public class DarthVader
+{
+    public string Speak()
+    {
+        return "I am your father.";
+    }
+}
+
+public class Luke : DarthVader
+{
+    public string Reply()
+    {
+        return "No! That's not true! That's impossible!";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Luke luke = new Luke();
+        Console.WriteLine(luke.Speak());
+        Console.WriteLine(luke.Reply());
+    }
+}
